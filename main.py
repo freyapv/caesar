c8f400e108d772321cfa9c4b057c4c35e4a30f50
freyapv <115910562+freyapv@users.noreply.github.com>
2023-02-04 15:41:34 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import art
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+print (art.logo)
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+        position = alphabet.index(char)
+        new_position = position + shift_amount
+        end_text += alphabet[new_position]
+    else:
+        end_text+=char
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+game_is_on=True
+while game_is_on:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift =shift %26
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    continue_the_code=input("Would you like to continue? Type 'yes' or 'no':\n").lower()
+    if continue_the_code=="no":
+        game_is_on=False
+        print("Goodbye")
+    
